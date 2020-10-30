4c6a00a0918ddac56cbb29ce8a46b5841257ba86
ichuan <iyanchuan@gmail.com>
2020-10-30 18:43:00 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@ LOTUS_CONFIG=/root/.lotus/config.toml
 SERVICE_CONFIG=/opt/filecoin-service.toml
 
 LOTUS_ADDR=${LOTUS_ADDR:-127.0.0.1:1234}
-LOTUS=/opt/coin/lotus-amd
 
 if grep 'model name' /proc/cpuinfo | grep -qi intel; then
-  LOTUS=/opt/coin/lotus-intel
+  ln -s /opt/coin/lotus-intel /opt/coin/lotus
+else
+  ln -s /opt/coin/lotus-amd /opt/coin/lotus
 fi
 
 mkdir -p /root/.lotus
@@ -19,8 +20,8 @@ if test $# -eq 0; then
       echo -e "[API]\nListenAddress=\"/ip4/0.0.0.0/tcp/1234/http\"" > $LOTUS_CONFIG
       echo -e "[Libp2p]\n[Pubsub]\n[Client]\n[Metrics]" >> $LOTUS_CONFIG
     fi
-    $LOTUS daemon &
-    # $LOTUS wait-api
+    /opt/coin/lotus daemon &
+    # /opt/coin/lotus wait-api
     echo 'Waiting for lotus JSON RPC...'
     while ! nc -z -w 1 127.0.0.1 1234; do
       sleep 1
