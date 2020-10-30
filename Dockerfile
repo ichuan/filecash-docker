1c836dbd58c5f5f4c6a37ed099fe2a7575b3aaae
ichuan <iyanchuan@gmail.com>
2020-10-30 11:52:27 +0800

    fix: build Step2

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,23 @@
 FROM rust:latest AS builder
 WORKDIR /opt
+# build filecoin-service
 RUN git clone https://github.com/Zondax/filecoin-signing-tools
 RUN cd filecoin-signing-tools && cargo build --release --manifest-path service/Cargo.toml
+# build lotus
+RUN apt update && apt install -y jq ocl-icd-opencl-dev
+RUN wget -c https://dl.google.com/go/go1.14.7.linux-amd64.tar.gz -O - | tar -xz -C /usr/local
+RUN git clone https://github.com/filecash/lotus_builder
+WORKDIR /opt/lotus_builder
+RUN PATH="$PATH:/usr/local/go/bin" bash build.sh -a
 
 FROM ubuntu:18.04
 WORKDIR /opt/coin
-RUN apt update && apt install -y wget mesa-opencl-icd libssl-dev netcat
-RUN wget https://github.com/filecash/lotus/releases/download/filecash-v0.7.0-S2/intel-filecash-S2.tar.gz -O - | tar -C /opt/coin -xzf -
+RUN apt update && apt install -y wget ocl-icd-opencl-dev libssl-dev netcat
 COPY --from=builder /opt/filecoin-signing-tools/target/release/filecoin-service /opt/coin/
+COPY --from=builder /opt/lotus_builder/lotus/lotus /opt/coin/
 COPY ./entrypoint.sh /opt/
 RUN chmod +x /opt/entrypoint.sh
 # cleanup
 RUN apt autoremove -y && rm -rf /var/lib/apt/lists/*
-RUN rm -f /opt/coin/lotus-miner /opt/coin/lotus-worker
 
 ENTRYPOINT ["/opt/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,29 @@
 LOTUS_CONFIG=/root/.lotus/config.toml
 SERVICE_CONFIG=/opt/filecoin-service.toml
 
+LOTUS_ADDR=${LOTUS_ADDR:-127.0.0.1:1234}
+
+mkdir -p /root/.lotus
+
 if test $# -eq 0; then
-  # generate lotus config.toml
-  if [ ! -f $LOTUS_CONFIG ]; then
-    echo -e "[API]\nListenAddress=\"/ip4/0.0.0.0/tcp/1234/http\"" > $LOTUS_CONFIG
-    echo -e "[Libp2p]\n[Pubsub]\n[Client]\n[Metrics]" >> $LOTUS_CONFIG
+  if [[ -z ${WITHOUT_NODE} ]]; then
+    # generate lotus config.toml
+    if [ ! -f $LOTUS_CONFIG ]; then
+      echo -e "[API]\nListenAddress=\"/ip4/0.0.0.0/tcp/1234/http\"" > $LOTUS_CONFIG
+      echo -e "[Libp2p]\n[Pubsub]\n[Client]\n[Metrics]" >> $LOTUS_CONFIG
+    fi
+    /opt/coin/lotus daemon &
+    # /opt/coin/lotus wait-api
+    echo 'Waiting for lotus JSON RPC...'
+    while ! nc -z -w 1 127.0.0.1 1234; do
+      sleep 1
+    done
   fi
-  /opt/coin/lotus daemon &
-  # /opt/coin/lotus wait-api
-  echo 'Waiting for lotus JSON RPC...'
-  while ! nc -z -w 1 127.0.0.1 1234; do
-    sleep 1
-  done
   # generate filecoin-service.toml
   JWT=`cat /root/.lotus/token`
+  LOTUS_TOKEN=${LOTUS_TOKEN:-JWT}
   echo -e "[service]\naddress=\"0.0.0.0:3030\"" > $SERVICE_CONFIG
-  echo -e "\n[remote_node]\nurl=\"http://127.0.0.1:1234/rpc/v0\"\njwt=\"$JWT\"" >> $SERVICE_CONFIG
+  echo -e "\n[remote_node]\nurl=\"http://${LOTUS_ADDR}/rpc/v0\"\njwt=\"${LOTUS_TOKEN}\"" >> $SERVICE_CONFIG
   exec /opt/coin/filecoin-service --config $SERVICE_CONFIG start
 else
   exec $@
